0b1dd520b3751dc9ab2ba080f026efe762d909eb
Nathan Clevenger <nateclev@gmail.com>
2022-07-25 03:02:03 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 export default {
   fetch: async (req, env, ctx) => {
     const pattern = new URLPattern({pathname: '/foo/:image.jpg'})
-    const result = p.exec({pathname: '/foo/cat.jpg'})
+    const result = pattern.exec({pathname: '/foo/cat.jpg'})
     
     const headers = {
       authorization: req.headers.get('authorization')
